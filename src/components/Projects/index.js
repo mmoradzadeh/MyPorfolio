95a1d3bae91a16294c7445a8d71695b768cf96b0
Mohammadmahdi Moradzadeh <126528880+Mahdi-Moradzadeh@users.noreply.github.com>
2023-08-12 16:15:58 -0400

    added projects to it

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,10 +1,35 @@
 import React from 'react';
+import Project2B from '../../assets/img/Project2B.jpg';
+import Project1 from '../../assets/img/Project1.jpg';
 
 function Projects() {
     return (
         <div className="projects" id='projects'>
             <h2>My Projects</h2>
-            <p>my projects content here...</p>
+            
+            <div className="dropdown">
+                <button 
+                    className="btn btn-secondary dropdown-toggle" 
+                    type="button"
+                    id="projectDropdown" 
+                    data-toggle="dropdown" 
+                    aria-haspopup="true" 
+                    aria-expanded="false"
+                >
+                    My Recent Projects
+                </button>
+                <div className="dropdown-menu" aria-labelledby="projectDropdown">
+                    <a className="dropdown-item" href="https://github.com/TimeBytes/Movie-Dashboard" target="_blank" rel="noopener noreferrer">
+                        Project 1
+                    </a>
+                    <img src={Project1} alt="project_one" style={{width: '500px', height: '200px'}} />
+
+                    <a className="dropdown-item" href="https://github.com/DanialDevelops/movie-review" target="_blank" rel="noopener noreferrer">
+                        Project 2
+                    </a>
+                    <img src={Project2B} alt="project_two" style={{width: '500px', height: '200px'}} />
+                </div>
+            </div>
         </div>
     );
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 import App from './App';
 
 const root = document.getElementById('root');
